946c2ca650248a418949b6994dd848827f5c6e58
avijeetd <avijeetd@gmail.com>
2014-03-14 20:41:12 +0530

    v1.0
    for phonegap build

---
+++ app/js/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
+  config(['$routeProvider', '$httpProvider', function($routeProvider,$httpProvider) {
+    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+    $routeProvider.when('/view3', {templateUrl: 'partials/partial3.html', controller: 'MyCtrl3'});
+    $routeProvider.otherwise({redirectTo: '/view1'});
+    
+    //Enable cross domain calls
+    $httpProvider.defaults.useXDomain = true;
+    //Remove the header used to identify ajax call  that would prevent CORS from working
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+  }]);
+
+
---
+++ app/js/controllers.js
@@ -0,0 +1,147 @@
+'use strict';
+
+var myappControllers =  angular.module('myApp.controllers', []);
+
+  myappControllers.controller('MyCtrl1',[ "$scope", "$http", "$log",   function(scope, http, log ) {
+//	scope.expenses = [{"id":1,"amount":1234,"description":"namdhari","date":1384885800000},{"id":2,"amount":3000,"description":"total","date":1384885800000}];
+	scope.expenses = [];
+        scope.balances = [];
+        
+        scope.currentDate = new Date()
+        scope.day = scope.currentDate.getDate()
+        scope.month = scope.currentDate.getMonth() + 1
+        scope.year = scope.currentDate.getFullYear()
+        scope.currentdate = scope.year + "-" + scope.month + "-" + scope.day ;
+        
+        
+        
+	scope.getExpensesData = function(url, page,log) {
+			http.get(url, page).success(function(data, status, headers, config) {
+			scope.expenses = data;
+			console.log('get expense data success'+data);
+		}).error(function(data, status, headers, config) {
+			alert('Error retrieving data ' + status);
+		});
+	}
+        
+        scope.getBalanceData = function(url, page,log) {
+			http.get(url, page).success(function(data, status, headers, config) {
+			scope.balances = data;
+			console.log('get balance data success'+data);
+		}).error(function(data, status, headers, config) {
+			alert('Error retrieving data ' + status);
+		});
+	}
+        
+            
+	scope.getExpenses = function() {		
+		
+            
+            
+            scope.getExpensesData("http://localhost:8282/app/webresources/expenses?month="+scope.month, null);
+	}
+        
+        
+        scope.getBalances = function() {
+            
+                //get current month
+                //get all bucket types
+                
+		scope.getBalanceData("http://localhost:8282/app/webresources/expenses/balance?month="+scope.month, null);
+	}
+       
+  }]);
+  
+  myappControllers.controller('MyCtrl2',[ "$scope","$http", "$log", function(scope,http, log) {
+       
+        var currentDate = new Date()
+        var day = currentDate.getDate()
+        var month = currentDate.getMonth() + 1
+        var year = currentDate.getFullYear()
+        var currentdate = year + "-" + month + "-" + day ;
+        
+        scope.currentdate=currentdate;
+        
+        scope.postData = function(url, data,config) {
+			http.post(url, data).success(function(data, status, headers, config) {
+			console.log('post success'+ data );
+		}).error(function(data, status, headers, config) {
+			alert('Error posting data ' + data);
+		});
+	}
+        
+        //scope.expenseform = {"id":1,"amount":1234,"description":"namdhari","date":1384885800000};
+
+       scope.saveExpenses = function(){
+           scope.expenseform.id='1'; 
+           scope.expenseform.date=currentdate;
+            console.log(scope.expenseform);
+            scope.postData("http://localhost:8282/app/webresources/expenses", scope.expenseform);
+            scope.expenseform='';
+        }
+        
+        scope.buckets = [];
+        
+        scope.getData = function(url, page,log) {
+			http.get(url, page).success(function(data, status, headers, config) {
+			scope.buckets = data;
+			console.log('get Data success');
+		}).error(function(data, status, headers, config) {
+			alert('Error retrieving data ' + status);
+		});
+	}	
+	scope.getBuckets = function() {		
+		scope.getData("http://localhost:8282/app/webresources/bucketlimits", null);
+	}
+  }]);
+  
+   myappControllers.controller('MyCtrl3',[ "$scope","$http", "$log", function(scope,http, log) {
+       
+  
+        scope.postData = function(url, data,config) {
+			http.post(url, data).success(function(data, status, headers, config) {
+			console.log('post success'+ data );
+		}).error(function(data, status, headers, config) {
+			alert('Error posting data ' + data);
+		});
+	}
+        
+        //scope.bucketform = {"bucketTypeId":1,"bucketType":"Misc","monthlyLimit":25000};
+
+       scope.saveNewBucket = function(){
+           scope.bucketform.bucketTypeId='1'; 
+           console.log(scope.bucketform);
+           scope.postData("http://localhost:8282/app/webresources/bucketlimits", scope.bucketform);
+           scope.bucketform='';
+        }
+       
+        scope.updateBucket = function(){
+           
+           console.log(scope.bucketform);
+           scope.postData("http://localhost:8282/app/webresources/bucketlimits", scope.bucketform.bucketTypeId);
+        }
+        
+      
+        
+        scope.buckets = [];
+        
+        scope.getData = function(url, page,log) {
+			http.get(url, page).success(function(data, status, headers, config) {
+			scope.buckets = data;
+			console.log('get Data success');
+		}).error(function(data, status, headers, config) {
+			alert('Error retrieving data ' + status);
+		});
+	}
+        
+	scope.getBuckets = function() {		
+		scope.getData("http://localhost:8282/app/webresources/bucketlimits", null);
+	}
+        /*
+        scope.setBucket = function(bucket) {		
+	scope.bucketform = bucket;
+              //  scope.bucketform = bucket;
+	}
+        */
+    
+  }]);
